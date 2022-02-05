c6ef728591ec7121fd97959991d3ea1accda4d51
GeneralUZB-1898 <98801984+Azamjon-Dehqonov-1898@users.noreply.github.com>
2022-02-05 17:11:20 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,11 +1,15 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-kanallar = ["@furaga_ish_UZBda"]
-adminlar = [1240250960]
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+# kanallar = ["@furaga_ish_UZBda"]
+# adminlar = [1240250960]
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+import os
+BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
+ADMINS = list(os.environ.get('ADMINS'))
+IP = os.environ.get('ip')
