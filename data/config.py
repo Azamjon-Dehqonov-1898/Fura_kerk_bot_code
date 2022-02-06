8fcf7a482869e40d28375cdcaf80c1caeaf3a3f2
GeneralUZB-1898 <98801984+Azamjon-Dehqonov-1898@users.noreply.github.com>
2022-02-06 07:38:35 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,7 +4,7 @@
 # env = Env()
 # env.read_env()
 kanallar = ["@furaga_ish_UZBda"]
-# adminlar = [1240250960]
+adminlar = [1240250960]
 # # .env fayl ichidan quyidagilarni o'qiymiz
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
